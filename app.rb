8f29432900367547a0ab3458f7f7a4662d08c1d2
Matiin Axner <matiin.axner@gmail.com>
2025-11-26 16:18:17 +0100

    Första lektion
    Idag har jag lagt in så jag kan se databasen, lägga till nya uppgifter i databasen. Men jag har även lagt till en ta bort knapp och uppdatera knapp men inga av de funkar.

--- app.rb
+++ app.rb
@@ -9,7 +9,32 @@ require 'sinatra/reloader'
 
 # Routen /
 get '/' do
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
+    @results = db.execute("SELECT * FROM todos")
     slim(:index)
 end
+
+post('/todo') do # app.rb
+    name = params[:name] 
+    description = params[:description]
+
+    db = SQLite3::Database.new("db/todos.db")
+    db.execute("INSERT INTO todos (name, description) VALUES (?,?)", [name, description])
+    redirect('/')
+  end
+
+  post('/todo/:id/update') do
+    db.execute("UPDATE todos SET name=?, age=? WHERE id=?", [params[:name], params[:description]])
+    redirect('/')
+  end
+
+  post('/todo/:id/delete') do
+    db = SQLite3::Database.new("db/todos.db")
+    ta_bort = params[:id].to_i
+    db.execute("DELETE FROM todos WHERE id=?", ta_bort)
+    redirect('/')
+  end
+
 
 
