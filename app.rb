26c4d3bdadb8240ddbcf79c36163ffec51094992
Matiin Axner <matiin.axner@gmail.com>
2025-12-03 16:12:01 +0100

    Gjort klart update, fixat utseende på knappen

--- app.rb
+++ app.rb
@@ -15,6 +15,13 @@ get '/' do
     slim(:index)
 end
 
+get '/todos/:id/edit' do
+  db = SQLite3::Database.new("db/todos.db")
+  db.results_as_hash = true
+  @result = db.execute("SELECT * FROM todos WHERE id=?", params[:id].to_i).first
+  slim(:edit)
+end
+
 post('/todo') do # app.rb
     name = params[:name] 
     description = params[:description]
@@ -25,9 +32,16 @@ post('/todo') do # app.rb
   end
 
   post('/todo/:id/update') do
-    db.execute("UPDATE todos SET name=?, age=? WHERE id=?", [params[:name], params[:description]])
+    id = params[:id].to_i
+    name = params[:name]
+    description = params[:description]
+  
+    db = SQLite3::Database.new("db/todos.db")
+    db.execute("UPDATE todos SET name=?, description=? WHERE id=?", [name, description, id])
+  
     redirect('/')
   end
+  
 
   post('/todo/:id/delete') do
     db = SQLite3::Database.new("db/todos.db")
